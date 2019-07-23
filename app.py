836bbfe41382ab1b6f7d78f81e646a777c5e7255
Utsav Darlami <utsavdarlami17@gmail.com>
2019-07-23 07:52:04 +0545

    changes in home with new changehome html

--- app.py
+++ app.py
@@ -16,8 +16,9 @@ from threading import Thread
 
 
 # our news Scrapper 
-from Scrapper.All_scrap import business_scrap,AW_scrap,nep_scrap,sports_scrap,technology_scrap
+# from Scrapper.All_scrap import business_scrap,AW_scrap,nep_scrap,sports_scrap,technology_scrap
 # from nep_scrap import NewsScrapper 
+from totalscrap import TotalNewsScrap
 
 #others
 import json
@@ -38,27 +39,95 @@ mongo = PyMongo(app)
 """ all the routess """
 @app.route("/")
 def home():
-    return render_template('home.html')
+    news_sports           = mongo.db.totalNews.find({"Category": "SPORTS"})
+    news_tech             = mongo.db.totalNews.find({"Category": "TECH"})
+    news_business         = mongo.db.totalNews.find({"Category": "BUSINESS"})
+    news_worldnews        = mongo.db.totalNews.find({"Category": "WORLDNEWS"})
+    news_entertainment    = mongo.db.totalNews.find({"Category": "ENTERTAINMENT"})
+    news_politics         = mongo.db.totalNews.find({"Category": "POLITICS"})
+    news_science          = mongo.db.totalNews.find({"Category": "SCIENCE"})
+    news_arts             = mongo.db.totalNews.find({"Category": "ARTS_CULTURE"})
+    #single view
+    single_sports         = mongo.db.totalNews.find_one({"Category": "SPORTS"})
+    single_tech           = mongo.db.totalNews.find_one({"Category": "TECH"})
+    single_business       = mongo.db.totalNews.find_one({"Category": "BUSINESS"})
+    single_worldnews      = mongo.db.totalNews.find_one({"Category": "WORLDNEWS"})
+    single_entertainment  = mongo.db.totalNews.find_one({"Category": "ENTERTAINMENT"})
+    single_politics       = mongo.db.totalNews.find_one({"Category": "POLITICS"})
+    single_science        = mongo.db.totalNews.find_one({"Category": "SCIENCE"})
+    single_arts           = mongo.db.totalNews.find_one({"Category": "ARTS_CULTURE"})
+
+    return render_template('changehome.html',
+                            news_sports=news_sports[1:6],
+                            news_tech=news_tech[1:6],
+                            news_business=news_business[1:6],
+                            news_worldnews=news_worldnews[1:6],
+                            news_entertainment=news_entertainment[1:6],
+                            news_politics=news_politics[1:6],
+                            news_science=news_science[1:6],
+                            news_arts=news_arts[1:6],
+                            single_sports=single_sports,
+                            single_tech=single_tech,
+                            single_business=single_business,
+                            single_worldnews=single_worldnews,
+                            single_entertainment=single_entertainment,
+                            single_politics=single_politics,
+                            single_science=single_science,
+                            single_arts=single_arts
+                        )
 #category urls
 @app.route("/sports/")
 def sports():
-    news_list = mongo.db.sportsNews.find().sort("Publish_Date")
+    news_list = mongo.db.totalNews.find({"Category": "SPORTS"})
     #news_list = mongo.db.allNews.find()
 
     return render_template('db.html',news_list =news_list)
 
 @app.route("/technology/")
 def technology():
-    news_list = mongo.db.techologyNews.find().sort("Publish_Date")
+    news_list = mongo.db.totalNews.find({"Category": "TECH"})
     #news_list = mongo.db.allNews.find()
 
     return render_template('db.html',news_list =news_list)
 
 @app.route("/business/")
 def business():
-    news_list = mongo.db.businessNews.find().sort("Publish_Date")
+    news_list = mongo.db.totalNews.find({"Category": "BUSINESS"})
     #news_list = mongo.db.allNews.find()
     return render_template('db.html',news_list =news_list)
+
+@app.route("/worldnews/")
+def worldnews():
+    news_list = mongo.db.totalNews.find({"Category": "WORLDNEWS"})
+    #news_list = mongo.db.allNews.find()
+    return render_template('db.html',news_list =news_list)
+
+@app.route("/entertainment/")
+def entertainment():
+    news_list = mongo.db.totalNews.find({"Category": "ENTERTAINMENT"})
+    #news_list = mongo.db.allNews.find()
+    return render_template('db.html',news_list =news_list)
+@app.route("/politics/")
+def politics():
+    news_list = mongo.db.totalNews.find({"Category": "POLITICS"})
+    #news_list = mongo.db.allNews.find()
+
+    return render_template('db.html',news_list =news_list)
+
+@app.route("/science/")
+def science():
+    news_list = mongo.db.totalNews.find({"Category": "SCIENCE"})
+    #news_list = mongo.db.allNews.find()
+
+    return render_template('db.html',news_list =news_list)
+
+
+@app.route("/arts_culture/")
+def arts_culture():
+    news_list = mongo.db.totalNews.find({"Category": "ARTS_CULTURE"})
+    #news_list = mongo.db.allNews.find()
+    return render_template('db.html',news_list =news_list)
+
 
 #---end---
 @app.route("/api/")
@@ -70,7 +139,7 @@ def test1():
 
     return render_template('index.html',data =(data))
 
-@app.route("/api2/")
+@app.route("/nepal/")
 def test2():
     news_list = mongo.db.allNews.find().sort("Publish_Date")
     #news_list = mongo.db.allNews.find()
@@ -92,10 +161,18 @@ def test3():
 
 @app.route("/api3/")
 def test3():
-    international_news =mongo.db.newNews.find().sort("Publish_Date")
+    international_news = mongo.db.filteredNews.find({"Category_Class": "WORLDNEWS"})
+
     return render_template('db.html',news_list =international_news)
+"""-------------------------"""
+@app.route("/pre/")
+def predict():
+    news_list = mongo.db.filteredNews.find({"Category_Class": "ENTERTAINMENT"})
+    #news_list = mongo.db.allNews.find()
 
+    return render_template('db.html',news_list =news_list)
 
+"""---------s"""
 @app.route("/about/")
 def about():
     return "About!"
@@ -138,11 +215,11 @@ if __name__=='__main__':
 
     #hours
 
-    schedule.every(6).hours.do(nep_scrap)
-    schedule.every(6).hours.do(AW_scrap)
-    schedule.every(6).hours.do(technology_scrap)
-    schedule.every(6).hours.do(sports_scrap)
-    schedule.every(6).hours.do(business_scrap)
+    schedule.every(6).hours.do(TotalNewsScrap)
+    # schedule.every(6).hours.do(AW_scrap)
+    # schedule.every(6).hours.do(technology_scrap)
+    # schedule.every(6).hours.do(sports_scrap)
+    # schedule.every(6).hours.do(business_scrap)
 
     #minutes
     # schedule.every(10).minutes.do(nep_scrap)
